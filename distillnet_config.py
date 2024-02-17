fd4e7ce4a92d9e4f396ef139efa85ba7845c01a1
Tristan Brandes <tristan.brandes@student.kit.edu>
2024-02-17 14:19:13 +0100

    Add comments

--- distillnet_config.py
+++ distillnet_config.py
@@ -1,36 +1,32 @@
 hparams = {
-            'batch_size': 256,
-            'maketrain_particles': 1.4e6,
-            'L1_hsize': 128,
-            'L2_hsize': 64,
-            'n_outputs': 1,
-            'lr': 0.0015,
-            }
-
-trainparams = {
-            'n_epochs': 2,
-            "patience": 5,
-            "best_loss": 0.17,
-            'weightedlossval': 3,
-            "train_sample": "distill_wjets_emd_prl.h5", #sample for training distill_wjets_swd.h5
-            "test_sample": "distill_ttbar_emd_prl.h5",  #sample for testing distill_ttbar_swd.h5
-            'bestmodel_losstype': 'bestmodel_trainloss' # choose whether to calculate physics results on bestmodel_trainloss or bestmodel_valloss
-            }
-
-
-dirs = {
-        'filedir': '/work/tbrandes/work/data/',
-        'savedir': '/work/tbrandes/delme/'
-    
+    "batch_size": 256,  # Batch size
+    "maketrain_particles": 1.4e6,  # Particles utlized for training
+    "L1_hsize": 128,  # Size Hidden Layer 1
+    "L2_hsize": 64,  # Size Hidden Layer 2
+    "n_outputs": 1,  # Number of output classes
+    "lr": 0.0015,  # Learning rate
 }
 
+trainparams = {
+    "n_epochs": 2,
+    "patience": 5,
+    "best_loss": 0.17,
+    "weightedlossval": 3,  # Weight of Weighted MAE Loss
+    "train_sample": "distill_wjets_emd_prl.h5",  # sample for training distill_wjets_swd.h5
+    "test_sample": "distill_ttbar_emd_prl.h5",  # sample for testing distill_ttbar_swd.h5
+    "bestmodel_losstype": "bestmodel_trainloss",  # choose whether to calculate physics results on bestmodel_trainloss or bestmodel_valloss
+}
+
+
+dirs = {"filedir": "/work/tbrandes/work/data/", "savedir": "/work/tbrandes/delme/"}
+
 bool_val = {
-            "is_displayplots": False,
-            "is_savefig": True,
-            "is_remove_padding": True,
-            "is_min_max_scaler": False,
-            "is_standard_scaler": True,
-            "is_dtrans": False,
-            "is_do_taylor": True,
-            "is_weighted_error": True,
-            }
+    "is_displayplots": False,  # works depending on GUI, recommended is leaving this False and examining the saved figures
+    "is_savefig": True,  # Save figures in savedir
+    "is_remove_padding": True,  # removal of zero-padded particles in events, recommended to leave True, otherwise results degrade
+    "is_min_max_scaler": False,  # Use min max scaler for scaling input training data
+    "is_standard_scaler": True,  # Use standard scaler for scaling input training data
+    "is_dtrans": False,  # Trainsform Input data from variable d0 and dZ to be in Interval abs(d0 or dZ) < 1; to aid input normalization due to outliers
+    "is_do_taylor": True,  # Do Taylor analysis on input features
+    "is_weighted_error": True,  # Use weighted MAE Error in training
+}
